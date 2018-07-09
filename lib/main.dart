faa4dc10980ca2c266f61e627752ff78fec2b5ec
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-09 15:43:25 +0530

    rearrange colors

--- lib/main.dart
+++ lib/main.dart
@@ -50,21 +50,21 @@ class Page extends StatelessWidget {
 
 List<Color> colorList = [
   Colors.blue[900],
-  Colors.lightBlue[900],
   Colors.cyan[900],
-  Colors.teal[900],
+  Colors.lightBlue[900],
   Colors.green[900],
+  Colors.teal[900],
+  Colors.yellow[900],
   Colors.lightGreen[900],
   Colors.lime[900],
-  Colors.yellow[900],
-  Colors.amber[900],
   Colors.orange[900],
-  Colors.deepOrange[900],
-  Colors.red[900],
+  Colors.amber[900],
   Colors.pink[900],
-  Colors.purple[900],
+  Colors.deepOrange[900],
   Colors.deepPurple[900],
+  Colors.red[900],
   Colors.indigo[900],
+  Colors.purple[900],
 ];
 
 List<String> fontFamily = [
